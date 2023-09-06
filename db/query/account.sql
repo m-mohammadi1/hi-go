9f3b37f8fd0d94095d7b8ccc67e3bb3110517fd3
Mr. Programmer lin <m.mohammadi.developer@gmail.com>
2023-09-06 14:46:41 +0330

    add ci-cd

--- db/query/account.sql
+++ db/query/account.sql
@@ -8,6 +8,11 @@ SELECT *
 FROM accounts
 WHERE id = $1 LIMIT 1;
 
+
+-- name: GetAccountForUpdate :one
+SELECT *
+FROM accounts
+WHERE id = $1 LIMIT 1 FOR NO KEY UPDATE;
 -- name: ListAccounts :many
 SELECT *
 FROM accounts
@@ -20,6 +25,11 @@ UPDATE accounts
 set balance = $2
 WHERE id = $1 RETURNING *;
 
+
+-- name: AddAccountBalance :one
+UPDATE accounts
+set balance = balance + sqlc.arg(amount)
+WHERE id = sqlc.arg(id) RETURNING *;
 
 -- name: DeleteAccount :exec
 DELETE
